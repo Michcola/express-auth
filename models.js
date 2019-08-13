46b4fe0ff74e61bfc2fcdddc3ee1d342b376ece1
mich <843298603@qq.com>
2019-08-13 23:24:41 +0800

    complete login&jwt_token

--- models.js
+++ models.js
@@ -9,11 +9,11 @@ const UserSchema = new mongoose.Schema({
     password: { 
         type: String,
         set(val) {
-            return require('bcrypt').hashSync(val)
+            return require('bcrypt').hashSync(val, 10)
         }
     },
 })
-//23:49
+
 
 const User = mongoose.model('User', UserSchema)
 // User.db.dropCollection('users')
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const { User } = require('./models')
 const express = require('express')
+const jwt = require('jsonwebtoken')
 
 const app = express()
+const SECRET = 'asdasdanifneifef'
 app.use(express.json())
 
-app.get('/api/users', async(req, res) => {
+app.get('/api/users', async (req, res) => {
     const users = await User.find()
     res.send(users)
 })
 
-app.post('/api/register', async(req, res) => {
+app.post('/api/register', async (req, res) => {
     const user = await User.create({
         username: req.body.username,
         password: req.body.password
@@ -17,6 +19,55 @@ app.post('/api/register', async(req, res) => {
     res.send(user)
 })
 
-app.listen(3001 , () => {
+app.post('/api/login', async (req, res) => {
+    const user = await User.findOne({
+        username: req.body.username
+    })
+    if (!user) {
+        return res.status(422).send({
+            message: '用户名不存在'
+        })
+    }
+    const isPasswordValid = require('bcrypt').compareSync(
+        req.body.password,
+        user.password
+    )
+
+    if (!isPasswordValid) {
+        return res.status(422).send({
+            message: '密码无效'
+        })
+    }
+
+    // 生成token
+    const token = jwt.sign({
+        id: String(user._id),
+    }, SECRET)
+    res.send({
+        user,
+        token
+    })
+})
+
+const auth = async (req, res, next) => {
+    const raw = String(req.headers.authorization).split(' ').pop()
+    const { id } = jwt.verify(raw, SECRET)
+    req.user = await User.findById(id)
+    next()
+}
+
+app.get('/api/profile', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+// app.get('/api/orders', auth, async (req, res) => {
+//     const orders = await Orders.find().where({
+//         user: req.user
+//     })
+//     res.send(orders)
+// })
+
+
+app.listen(3001, () => {
     console.log('http://localhost:3001')
 })
